06602ab06565b1b7cf40ac55d932350fd8ac1509
Kevin Kish <kevinkish117@gmail.com>
2026-05-11 04:33:36 -0400

    Added student detail page

--- composeApp/src/commonMain/kotlin/com/drcmind/kelasisuite/ui/components/AppIcons.kt
+++ composeApp/src/commonMain/kotlin/com/drcmind/kelasisuite/ui/components/AppIcons.kt
@@ -1,29 +1,35 @@
 package com.drcmind.kelasisuite.ui.components
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.TrendingUp
 import androidx.compose.material.icons.automirrored.rounded.ArrowForward
 import androidx.compose.material.icons.rounded.Chat
 import androidx.compose.material.icons.rounded.Dashboard
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material.icons.rounded.Groups
+import androidx.compose.material.icons.rounded.HistoryEdu
 import androidx.compose.material.icons.rounded.MenuBook
 import androidx.compose.material.icons.rounded.Payments
 import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material.icons.rounded.Person2
 import androidx.compose.material.icons.rounded.PersonAdd
 import androidx.compose.material.icons.rounded.School
+import androidx.compose.material.icons.rounded.Verified
 
 object AppIcons {
 
-    val dashboard = Icons. Rounded.Dashboard
-        val curriculum = Icons.Rounded.MenuBook
-        val enrollment = Icons.Rounded.PersonAdd
-        val financial = Icons.Rounded.Payments
-        val communication = Icons.Rounded.Chat
-        val person = Icons.Rounded.Person
-        val peoples = Icons.Rounded.Groups
-        val personAdd = Icons.Rounded.PersonAdd
-        val school = Icons.Rounded.School
-        val payments = Icons.Rounded.Payments
-        val arrowFWD = Icons.AutoMirrored.Rounded.ArrowForward
-
+    val dashboard = Icons.Rounded.Dashboard
+    val curriculum = Icons.Rounded.MenuBook
+    val enrollment = Icons.Rounded.PersonAdd
+    val financial = Icons.Rounded.Payments
+    val communication = Icons.Rounded.Chat
+    val person = Icons.Rounded.Person
+    val peoples = Icons.Rounded.Groups
+    val personAdd = Icons.Rounded.PersonAdd
+    val school = Icons.Rounded.School
+    val payments = Icons.Rounded.Payments
+    val arrowFWD = Icons.AutoMirrored.Rounded.ArrowForward
+    val checkMark = Icons.Rounded.Verified
+    val edit = Icons.Rounded.Edit
+    val trending = Icons.AutoMirrored.Filled.TrendingUp
+    val educHistory =  Icons.Rounded.HistoryEdu
 }
