6aa4398bb8ee00f5a181c625f06fafdb8c89a63c
Kevin Kish <kevinkish117@gmail.com>
2026-05-25 11:22:27 +0200

    Merged with main

--- shared/src/jvmMain/kotlin/com/drcmind/kelasisuite/domain/util/Constant.jvm.kt
+++ shared/src/jvmMain/kotlin/com/drcmind/kelasisuite/domain/util/Constant.jvm.kt
@@ -1,4 +1,4 @@
 package com.drcmind.kelasisuite.domain.util
 
-actual val BASE_URL: String = "http://127.0.0.1:8080/api/v1/"
+actual val BASE_URL: String = "http://192.168.1.192:8080/api/v1/"
 //actual val BASE_URL: String = "https://kelasi-back end-production.up.railway.app/api/v1/"
