8453dd80aa91566c03496e44ab81ea0cbe23006a
ChristianAganze <165890534+ChristianAganze@users.noreply.github.com>
2026-09-03 12:57:00 +0200

    build: update project dependencies and SDK targets
    Upgrade Android SDK to 36, update Compose Multiplatform to 1.11.0, and bump various library versions to incorporate the latest features and bug fixes.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -51,7 +51,7 @@ kotlin {
             implementation(libs.compose.ui)
             implementation(libs.compose.material3)
             implementation(libs.compose.components.resources)
-            implementation(libs.compose.uiToolingPreview)
+            implementation(libs.compose.components.uiToolingPreview)
 
             implementation(libs.androidx.lifecycle.runtimeCompose)
 
